84ed2932a191963581488821bc0a6133acce3c6d
Thomas Jennison <thomas.a.jennison@gmail.com>
2021-01-30 10:06:47 -0500

    Added pages

---
+++ LogYourself/LogYourself/Pages/Logs/ActivityPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SelfMonitoringApp.Pages.Logs
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ActivityPage : ContentPage
+    {
+        public ActivityPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ LogYourself/LogYourself/Pages/PageNames.cs
@@ -0,0 +1,23 @@
+﻿namespace SelfMonitoringApp.Pages
+{
+    public enum PageNames
+    {
+        MoodEditor,
+        MealEditor,
+        SubstanceEditor,
+        SleepEditor,
+        ActivityEditor,
+        SocializationEditor,
+
+        RawLogViewer,
+        TrendsViewer,
+
+        NotificationsViewer,
+        NotificationsEditor,
+
+        GoalsViewer,
+        GoalsEditor,
+
+        Settings
+    }
+}
